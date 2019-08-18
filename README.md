982ed844d4b19f47ad9c5efae4c551f648e1339d
sohaibfb <sohaib.f.bataineh@gmail.com>
2019-08-18 12:37:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Decentralized Star Notary Project
+
+For this you will need to use the latest version of Truffle and Metamask
+
+Truffle v5.0.30 (core: 5.0.30)
+Solidity v0.5.0 (solc-js)
+Node v10.15.3
+Web3.js v^1.2.0
+
+did not know how to show OpenZeppelin version.
+
+
+
+ERC-721 Token Name :  My Token
+ERC-721 Token Symbol: SOH
---
+++ test/TestStarNotary.js
@@ -0,0 +1,150 @@
+const StarNotary = artifacts.require("StarNotary");
+
+var accounts;
+var owner;
+
+contract('StarNotary', (accs) => {
+    accounts = accs;
+    owner = accounts[0];
+});
+
+it('can Create a Star', async() => {
+    let tokenId = 1;
+    let instance = await StarNotary.deployed();
+    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]})
+    assert.equal(await instance.tokenIdToStarInfo.call(tokenId), 'Awesome Star!')
+});
+
+it('lets user1 put up their star for sale', async() => {
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let starId = 2;
+    let starPrice = web3.utils.toWei(".01", "ether");
+    await instance.createStar('awesome star', starId, {from: user1});
+    await instance.putStarUpForSale(starId, starPrice, {from: user1});
+    assert.equal(await instance.starsForSale.call(starId), starPrice);
+});
+
+it('lets user1 get the funds after the sale', async() => {
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 3;
+    let starPrice = web3.utils.toWei(".01", "ether");
+    let balance = web3.utils.toWei(".05", "ether");
+    await instance.createStar('awesome star', starId, {from: user1});
+    await instance.putStarUpForSale(starId, starPrice, {from: user1});
+    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
+    await instance.buyStar(starId, {from: user2, value: balance});
+    let balanceOfUser1AfterTransaction = await web3.eth.getBalance(user1);
+    let value1 = Number(balanceOfUser1BeforeTransaction) + Number(starPrice);
+    let value2 = Number(balanceOfUser1AfterTransaction);
+    assert.equal(value1, value2);
+});
+
+it('lets user2 buy a star, if it is put up for sale', async() => {
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 4;
+    let starPrice = web3.utils.toWei(".01", "ether");
+    let balance = web3.utils.toWei(".05", "ether");
+    await instance.createStar('awesome star', starId, {from: user1});
+    await instance.putStarUpForSale(starId, starPrice, {from: user1});
+    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
+    await instance.buyStar(starId, {from: user2, value: balance});
+    assert.equal(await instance.ownerOf.call(starId), user2);
+});
+
+it('lets user2 buy a star and decreases its balance in ether', async() => {
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 5;
+    let starPrice = web3.utils.toWei(".01", "ether");
+    let balance = web3.utils.toWei(".05", "ether");
+    await instance.createStar('awesome star', starId, {from: user1});
+    await instance.putStarUpForSale(starId, starPrice, {from: user1});
+    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
+    const balanceOfUser2BeforeTransaction = await web3.eth.getBalance(user2);
+    await instance.buyStar(starId, {from: user2, value: balance, gasPrice:0});
+    const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
+    let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
+    assert.equal(value, starPrice);
+});
+
+// Implement Task 2 Add supporting unit tests
+
+it('can add the star name and star symbol properly', async() => {
+
+    let instance = await StarNotary.deployed();
+    // 1. create a Star with different tokenId
+    let tokenId1=6;
+    let tokenId2=7;
+
+    await instance.createStar('Star1', tokenId1, {from: accounts[0]});
+    await instance.createStar('Star2', tokenId2, {from: accounts[0]});
+   
+    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+
+    assert.equal(await instance.Name.call(), 'My Token');
+    assert.equal(await instance.Symbol.call(), 'SOH');
+    
+});
+
+it('lets 2 users exchange stars', async() => {
+
+    let instance = await StarNotary.deployed();
+
+    // 1. create 2 Stars with different tokenId
+    let tokenId1=8;
+    let tokenId2=9;
+    let user1=accounts[1];
+    let user2=accounts[2];
+    await instance.createStar('Star1', tokenId1, {from: user1});
+    await instance.createStar('Star2', tokenId2, {from: user2});
+  
+    // 2. Call the exchangeStars functions implemented in the Smart Contract
+      await instance.exchangeStars(tokenId1,tokenId2,{from: accounts[1]});
+     
+    // 3. Verify that the owners changed
+    assert.equal(await instance.ownerOf.call(tokenId1),user2);
+    
+});
+
+it('lets a user transfer a star', async() => {
+
+    let instance = await StarNotary.deployed();
+
+    // 1. create a Star with different tokenId
+    let tokenId=10;
+    let user1=accounts[1];
+    let user2=accounts[2];
+    await instance.createStar('Star1', tokenId, {from: user1});
+
+    // 2. use the transferStar function implemented in the Smart Contract
+
+    
+    await instance.transferStar(user2,tokenId,{from: user1});
+
+    // 3. Verify the star owner changed.
+
+    assert.equal(await instance.ownerOf.call(tokenId),user2);
+});
+
+it('lookUptokenIdToStarInfo test', async() => {
+
+    let instance = await StarNotary.deployed();
+
+    // 1. create a Star with different tokenId
+    let tokenId=11;
+    let user1=accounts[1];
+    await instance.createStar('Star1', tokenId, {from: user1});
+
+    // 2. Call your method lookUptokenIdToStarInfo
+
+    
+
+    // 3. Verify if you Star name is the same
+    assert.equal(await instance.lookUptokenIdToStarInfo(tokenId),'Star1');
+});
